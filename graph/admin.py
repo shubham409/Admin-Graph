3e04048dedabf3adadb9b31c0d736926de40aa9b
shubham409 <sm091274@gmail.com>
2022-03-01 10:06:54 +0530

    Chart in admin with sample data

--- graph/admin.py
+++ graph/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import EmailSubscriber
+
+@admin.register(EmailSubscriber)
+class EmailSubscriberAdmin(admin.ModelAdmin):
+    list_display = ("id", "email", "created_at")
+    ordering = ("-created_at",) 
+    change_list_template ='graph/change_list.html'
+    
